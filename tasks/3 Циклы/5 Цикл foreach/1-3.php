db4344081cee0f4a2d1a231c496bc6912d74d054
EduHorog <90508178+EduHorog@users.noreply.github.com>
2023-04-07 22:53:12 +0500

    Add files via upload

---
+++ tasks/3 Циклы/5 Цикл foreach/1-3.php
@@ -0,0 +1,30 @@
+<?php
+	$arr = ['a', 'b', 'c', 'd', 'e'];
+    foreach ($arr as $elem) {
+        echo $elem . '<br>';
+    }
+?>
+
+<br>
+
+<?php
+	$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    $res = 0;
+    foreach ($arr as $elem) {
+        $res += $elem;
+    }
+
+    echo $res;
+?>
+
+<br>
+
+<?php
+	$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    $aaa = 9;
+    $res = 0;
+    foreach ($arr as $elem) {
+        $res += $elem;
+    }
+    echo $res / $aaa;
+?>
